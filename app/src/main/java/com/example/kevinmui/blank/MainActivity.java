6ff129f21e51236340ff916fbfcf1ade77961379
kmui2 <kmui2@wisc.edu>
2017-08-22 22:32:10 -0500

    first commit

---
+++ app/src/main/java/com/example/kevinmui/blank/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.kevinmui.blank;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTitle("Are You a Sloth?");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView scoreTextView = (TextView) findViewById(R.id.scoreTextView);
+        final EditText gpaText = (EditText) findViewById(R.id.gpaText);
+        final EditText sleepText = (EditText) findViewById(R.id.sleepText);
+        Button calcButton = (Button) findViewById(R.id.calcButton);
+        calcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float gpa = Float.parseFloat(gpaText.getText().toString());
+                float sleep = Float.parseFloat(sleepText.getText().toString());
+                float score = ((6*(4-gpa)+sleep)/48)*100;
+                scoreTextView.setText(Float.toString(score)+"/100 points");
+                gpaText.setText("");
+                sleepText.setText("");
+            }
+        });
+    }
+}
